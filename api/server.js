54a53707161ea8e4bab64ab34a49bb6aa7296a4c
cFoschini <cjfoschini25@gmail.com>
2022-01-23 23:10:29 -0500

    finished

--- api/server.js
+++ api/server.js
@@ -1,9 +1,5 @@
-const express = require('express');
-const server = express();
-
-// Configure your server here
-// Build your actions router in /api/actions/actions-router.js
-// Build your projects router in /api/projects/projects-router.js
-// Do NOT `server.listen()` inside this file!
-
+const express = require('express')
+const server = express()
+const projectsRouter = require('./projects/projects-router')
+const actionsRouter = require('./actions/actions-router')
 module.exports = server;
